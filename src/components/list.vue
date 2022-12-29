2612330f5dc7aaf4c6bd548e5e9845c5382352fa
anwar254 <sadatt254@gmail.com>
2022-12-29 06:42:52 +0300

    mark as done is complete

--- src/components/list.vue
+++ src/components/list.vue
@@ -1,20 +1,53 @@
 <script lang="ts">
 
+// { id:'1', message: 'Learn view js' }, 
+//                 { id:'2', message: 'Work on the delete functionality' }, 
+//                 { id:'3', message: 'Go to bed' }, 
+//                 { id:'4', message: 'Create a todo drag functionality' }, 
+//                 { id:'5', message: 'Create a input for adding more items' }, 
+//                 { id:'6', message: 'create the todo list app backend' }, 
+//                 { id:'7', message: 'Create the todolist google chrome extension', completed: true },
+
 export default {
     data() {
         return {
-            isDone: false,
             parentMessage: 'Parent',
-            todos: [{ id:'1', message: 'Learn view js' }, { id:'2', message: 'Work on the delete functionality' },{ id:'3', message: 'Go to bed' }, ]
+            total_todos: 0,
+            todos: [
+                { id:'1', message: 'Learn view js', completed: true }, 
+                { id:'2', message: 'Work on the delete functionality', completed: true }, 
+                { id:'3', message: 'Go to bed', completed: true }, 
+                { id:'4', message: 'Create a todo drag functionality', completed: true }, 
+                { id:'5', message: 'Create a input for adding more items', completed: true }, 
+                { id:'6', message: 'create the todo list app backend', completed: true }, 
+                { id:'7', message: 'Create the todolist google chrome extension', completed: true },
+
+            ]
         }
     },
     methods: {
-        markDone () {
-            this.isDone = !this.isDone
+        getObject (text,completed) {
+			return function () {
+				return {
+					text: text,
+					completed: completed
+				};
+			}
+		},
+        markDone (index) {
+            const object = this.todos[index];
+            if (object.completed) {
+                this.todos[index].completed = false;
+			}else {
+                this.todos[index].completed = true;
+                
+			}
+			this.total_todos = this.todos.length;
         },
 
         deleteItem(index) {
             this.todos.splice(index, 1);
+            this.total_todos = this.todos.length;
             console.log(`${index}th element deleted! `);
         }
     }
@@ -25,15 +58,15 @@ export default {
 props: ['todo'],
 
 <template>
-    <li class="py-3 sm:py-4"  v-for="(todo, index) in todos" :key="todo.message">
+    <li class="py-3 sm:py-4 hover:text-purple hover:font-black"  v-for="(todo, index) in todos" :key="todo.id">
         <div class="flex items-center ">
             <div class="flex-1 min-w-0">
-                <p class="text-sm font-medium truncate" :class="[ isDone ? 'text-gray-light line-through' : 'text-gray' ]">
+                <p class="text-sm font-medium truncate" :class="[ todo.completed ? 'text-gray-light line-through' : 'text-gray' ]">
                     {{ todo.message }}
                 </p>
             </div>
-            <div class="inline-flex items-center text-base font-semibold text-gray-900" @click="markDone">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" v-if="isDone" class="w-6 h-6 text-green done transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white cursor-pointer">
+            <div class="inline-flex items-center text-base font-semibold text-gray-900" @click="markDone(index)">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" v-if="todo.completed" class="w-6 h-6 text-green done transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white cursor-pointer">
                     <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
                 </svg>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" v-else class="w-6 h-6 text-green done transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white cursor-pointer">
